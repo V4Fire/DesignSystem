a5ab44b6b1eb598716009cb8b38290df53be31f1
dmitrybrovka <dmitrybrovka@yandex.ru>
2019-08-13 17:24:43 +0300

    Added units

--- index.js
+++ index.js
@@ -7,83 +7,72 @@ module.exports = {
           s: {
             placeholderColor: "rgba(108, 117, 125, 1)",
             placeholderStyle: "small",
-            horOffset: 7,
-            vertOffset: 14,
-            textHeight: 16,
-            height: 31,
+            horOffset: "7px",
+            vertOffset: "14px",
+            textHeight: "16px",
+            height: "31px",
             textStyle: "small"
           },
           m: {
             placeholderColor: "rgba(108, 117, 125, 1)",
             placeholderStyle: "base",
-            horOffset: 10,
-            vertOffset: 14,
-            textHeight: 16,
-            height: 38,
+            horOffset: "10px",
+            vertOffset: "14px",
+            textHeight: "16px",
+            height: "38px",
             textStyle: "base"
           },
           l: {
             placeholderColor: "rgba(108, 117, 125, 1)",
             placeholderStyle: "big",
-            horOffset: 12,
-            vertOffset: 14,
-            textHeight: 23,
-            height: 48,
+            horOffset: "12px",
+            vertOffset: "14px",
+            textHeight: "23px",
+            height: "48px",
             textStyle: "big"
           }
         },
         focus: {
-          shadowColor: "rgba(0, 123, 255, 0.25)",
-          shadowOffset: { x: 0, y: 0 },
-          shadowBlur: 3.200000047683716,
-          borderWidth: 1,
-          borderColor: "rgba(128, 189, 255, 1)"
+          boxShadow: "0px 0px 3.200000047683716px rgba(0, 123, 255, 0.25)",
+          border: "1px solid rgba(128, 189, 255, 1)"
         },
         valid: {
           valid: {
-            borderWidth: 1,
-            borderColor: "rgba(40, 167, 69, 1)",
+            border: "1px solid rgba(40, 167, 69, 1)",
             focus: {
-              shadowColor: "rgba(40, 167, 69, 0.25)",
-              shadowOffset: { x: 0, y: 0 },
-              shadowBlur: 3.200000047683716,
-              borderWidth: 1,
-              borderColor: "rgba(40, 167, 69, 1)"
+              boxShadow: "0px 0px 3.200000047683716px rgba(40, 167, 69, 0.25)",
+              border: "1px solid rgba(40, 167, 69, 1)"
             },
             info: {
               textStyle: "small",
               color: "rgba(40, 167, 69, 1)",
-              offset: { top: 4, left: 0 }
+              offset: { top: "4px", left: "0px" }
             },
             icon: {
               name: "ok",
-              offset: { top: 11, right: 9 },
-              width: 18,
-              height: 15.34959602355957,
+              offset: { top: "11px", right: "9px" },
+              width: "18px",
+              height: "15.34959602355957px",
               color: "rgba(40, 167, 69, 1)"
             }
           },
           invalid: {
-            borderWidth: 1,
-            borderColor: "rgba(220, 53, 69, 1)",
+            border: "1px solid rgba(220, 53, 69, 1)",
             focus: {
-              shadowColor: "rgba(220, 53, 69, 0.25)",
-              shadowOffset: { x: 0, y: 0 },
-              shadowBlur: 3.200000047683716,
-              borderWidth: 1,
-              borderColor: "rgba(220, 53, 69, 1)"
+              boxShadow: "0px 0px 3.200000047683716px rgba(220, 53, 69, 0.25)",
+              border: "1px solid rgba(220, 53, 69, 1)"
             },
             icon: {
               name: "close",
-              offset: { top: 13, right: 9 },
-              width: 12,
-              height: 12,
+              offset: { top: "13px", right: "9px" },
+              width: "12px",
+              height: "12px",
               color: "rgba(220, 53, 69, 1)"
             },
             info: {
               textStyle: "small",
               color: "rgba(220, 53, 69, 1)",
-              offset: { top: 4, left: 0 }
+              offset: { top: "4px", left: "0px" }
             }
           }
         },
@@ -93,18 +82,20 @@ module.exports = {
           baseBorderColor: "rgba(207, 212, 217, 1)"
         },
         preIcon: {
-          iconSize: 11,
-          offset: 13,
-          baseBgColor: "rgba(233, 236, 239, 1)",
-          baseBorderWidth: 1,
-          baseBorderColor: "rgba(207, 212, 217, 1)"
+          iconSize: "11px",
+          offset: "13px",
+          base: {
+            bgColor: "rgba(233, 236, 239, 1)",
+            border: { width: 1, color: "rgba(207, 212, 217, 1)" }
+          }
         },
         postIcon: {
-          iconSize: 11,
-          offset: 13,
-          baseBgColor: "rgba(233, 236, 239, 1)",
-          baseBorderWidth: 1,
-          baseBorderColor: "rgba(207, 212, 217, 1)"
+          iconSize: "11px",
+          offset: "13px",
+          base: {
+            bgColor: "rgba(233, 236, 239, 1)",
+            border: { width: 1, color: "rgba(207, 212, 217, 1)" }
+          }
         }
       }
     },
@@ -118,72 +109,245 @@ module.exports = {
         },
         hover: {
           blendMode: "color-burn",
-          opacity: 0.41999998688697815,
-          backgroundColor: "rgba(186, 186, 186, 0.41999998688697815)"
+          opacity: 0.8999999761581421,
+          backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
         },
         disabled: {
           blendMode: "normal",
           opacity: 0.41999998688697815,
           backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
         },
-        preIcon: { iconSize: 12, offset: 4 },
-        postIcon: { iconSize: 12, offset: 4 },
+        preIcon: { iconSize: "12px", offset: "4px" },
+        postIcon: { iconSize: "12px", offset: "4px" },
         size: {
           m: {
-            horOffset: 11,
-            vertOffset: 8,
-            textHeight: 17,
-            height: 38,
+            horOffset: "11px",
+            vertOffset: "8px",
+            textHeight: "17px",
+            height: "38px",
             textStyle: "base"
           },
           s: {
-            horOffset: 11,
-            vertOffset: 8,
-            textHeight: 10,
-            height: 31,
+            horOffset: "11px",
+            vertOffset: "8px",
+            textHeight: "10px",
+            height: "31px",
             textStyle: "small"
           },
           l: {
-            horOffset: 11,
-            vertOffset: 8,
-            textHeight: 27,
-            height: 48,
+            horOffset: "11px",
+            vertOffset: "8px",
+            textHeight: "27px",
+            height: "48px",
             textStyle: "big"
           }
+        },
+        focus: {
+          blendMode: "color-burn",
+          opacity: 0.8999999761581421,
+          backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
         }
       },
       exterior: {
         dark: {
+          backgroundColor: "rgba(52, 58, 64, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(52, 58, 64, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         light: {
+          backgroundColor: "rgba(248, 249, 250, 1)",
           color: "rgba(33, 37, 41, 1)",
-          backgroundColor: "rgba(248, 249, 250, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         info: {
+          backgroundColor: "rgba(23, 162, 184, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(23, 162, 184, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         warning: {
+          backgroundColor: "rgba(255, 193, 7, 1)",
           color: "rgba(33, 37, 41, 1)",
-          backgroundColor: "rgba(255, 193, 7, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         danger: {
+          backgroundColor: "rgba(220, 53, 69, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(220, 53, 69, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         success: {
+          backgroundColor: "rgba(40, 167, 69, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(40, 167, 69, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         secondary: {
+          backgroundColor: "rgba(108, 117, 125, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(108, 117, 125, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         },
         primary: {
+          backgroundColor: "rgba(0, 123, 255, 1)",
           color: "rgba(255, 255, 255, 1)",
-          backgroundColor: "rgba(0, 123, 255, 1)"
+          active: {
+            blendMode: "color-burn",
+            backgroundColor: "rgba(164, 164, 164, 1)"
+          },
+          hover: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          focus: {
+            blendMode: "color-burn",
+            opacity: 0.8999999761581421,
+            backgroundColor: "rgba(186, 186, 186, 0.8999999761581421)"
+          },
+          disabled: {
+            blendMode: "normal",
+            opacity: 0.41999998688697815,
+            backgroundColor: "rgba(255, 255, 255, 0.41999998688697815)"
+          },
+          preIcon: { iconSize: "12px", offset: "4px" },
+          postIcon: { iconSize: "12px", offset: "4px" }
         }
       }
     }
@@ -212,105 +376,115 @@ module.exports = {
       {
         type: "TEXT",
         name: "Heading/1",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 40,
-        letterSpacing: 0,
-        lineHeight: "46.875px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "40px",
+          lineHeight: "46.875px"
+        },
         color: "rgba(33, 37, 41, 1)"
       },
       {
         type: "TEXT",
         name: "Heading/2",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 32,
-        letterSpacing: 0,
-        lineHeight: "37.5px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "32px",
+          lineHeight: "37.5px"
+        },
         color: "rgba(33, 37, 41, 1)"
       },
       {
         type: "TEXT",
         name: "Heading/3",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 28,
-        letterSpacing: 0,
-        lineHeight: "32.8125px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "28px",
+          lineHeight: "32.8125px"
+        },
         color: "rgba(33, 37, 41, 1)"
       },
       {
         type: "TEXT",
         name: "Heading/4",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 24,
-        letterSpacing: 0,
-        lineHeight: "28.125px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "24px",
+          lineHeight: "28.125px"
+        },
         color: "rgba(33, 37, 41, 1)"
       },
       {
         type: "TEXT",
         name: "Heading/5",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 20,
-        letterSpacing: 0,
-        lineHeight: "23.4375px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "20px",
+          lineHeight: "23.4375px"
+        },
         color: "rgba(33, 37, 41, 1)"
       },
       {
         type: "TEXT",
         name: "Heading/6",
-        fontFamily: "Roboto",
-        fontWeight: 400,
-        fontSize: 16,
-        letterSpacing: 0,
-        lineHeight: "18.75px",
+        style: {
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "16px",
+          lineHeight: "18.75px"
+        },
         color: "rgba(33, 37, 41, 1)"
       }
     ],
     base: {
       type: "TEXT",
       name: "Base",
-      fontFamily: "Roboto",
-      fontWeight: 400,
-      fontSize: 16,
-      letterSpacing: 0,
-      lineHeight: "16px",
+      style: {
+        fontFamily: "Roboto",
+        fontWeight: 400,
+        fontSize: "16px",
+        lineHeight: "16px"
+      },
       color: "rgba(33, 37, 41, 1)"
     },
     small: {
       type: "TEXT",
       name: "Small",
-      fontFamily: "Roboto",
-      fontWeight: 400,
-      fontSize: 14,
-      letterSpacing: 0,
-      lineHeight: "16.40625px",
+      style: {
+        fontFamily: "Roboto",
+        fontWeight: 400,
+        fontSize: "14px",
+        lineHeight: "16.40625px"
+      },
       color: "rgba(33, 37, 41, 1)"
     },
     big: {
       type: "TEXT",
       name: "Big",
-      fontFamily: "Roboto",
-      fontWeight: 400,
-      fontSize: 20,
-      letterSpacing: 0,
-      lineHeight: "23.4375px",
+      style: {
+        fontFamily: "Roboto",
+        fontWeight: 400,
+        fontSize: "20px",
+        lineHeight: "23.4375px"
+      },
       color: "rgba(33, 37, 41, 1)"
     },
     link: {
       type: "TEXT",
       name: "Link",
-      fontFamily: "Roboto",
-      fontWeight: 400,
-      fontSize: 16,
-      letterSpacing: 0,
-      textDecoration: "underline",
-      lineHeight: "18.75px",
+      style: {
+        fontFamily: "Roboto",
+        fontWeight: 400,
+        fontSize: "16px",
+        textDecoration: "underline",
+        lineHeight: "18.75px"
+      },
       color: "rgba(0, 123, 255, 1)"
     }
   },
-  rounding: { big: 10, small: 4 }
+  rounding: { big: "10px", small: "4px" }
 };
